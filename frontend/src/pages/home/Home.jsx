6bd05ac0adae2e10cd8d6bcf79f715683b6381c0
Shewon Gunarathne <shewongunarathne@gmail.com>
2025-03-09 17:21:11 +0530

    commit

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
+import Recommended from './Recommended'
+import News from './news'
 
 const Home = () => {
   return (
     <>
     <Banner/>
     <TopSellers/>
+    <Recommended/>
+    <News/>
     </>
   )
 }
