8811dab8ca05c8c7b7dde72b93cc0c94d2575b72
Shewon Gunarathne <shewongunarathne@gmail.com>
2025-03-11 19:48:21 +0530

    commit

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -133,7 +133,7 @@ const CheckoutPage = () => {
                                 <div className="md:col-span-5 text-right">
                                     <div className="inline-flex items-end">
                                         <button 
-                                        disabled={!isChecked}
+                                        //disabled={!isChecked}
                                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Place an Order</button>
                                     </div>
                                 </div>
